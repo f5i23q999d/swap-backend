965308e800d02919c5ba8a193b5b5b0011ea3f7b
Kunhong.Li <knhongli@gmail.com>
2023-04-27 17:54:46 +0800

    初步重构

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 module.exports = {
     "rpc" : "http://127.0.0.1:8545",
     "port" : 3000,
-    "FXSWAP_ADDRESS" : "0xAb3bcE27F31Ca36AAc6c6ec2bF3e79569105ec2c"
+    "FXSWAP_ADDRESS" : "0xc027a4dBb9E76Ad7FC73fF2485ea9e99A9b90B3E",
+    "uniswapV2Handler_ADDRESS":"0xa0eEcC69b884E29d2C44e0d9c0C88f41A8eb54B3",
+    "uniswapV3Handler_ADDRESS":"0xb3f25e96f34CCD7f21EE58272308513B4E0633e4",
+    "aaveV2Handler_ADDRESS":"0xE06d76d72Db4A9763592F4aE741C662edbe8902c"
 
 }
