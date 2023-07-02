f13682f871b1313ab8f022f66c00435a20762d0e
Kunhong.Li <knhongli@gmail.com>
2023-07-02 11:38:42 +0800

    fix: 0x test add chainId parameter

--- test/test_0x.js
+++ test/test_0x.js
@@ -9,63 +9,62 @@ const ERC20ABI = require('../helpers/abi/ERC20.json');
 const ADAI_ADDRESS = '0x028171bCA77440897B824Ca71D1c56caC55b68A3';
 const axios = require('axios');
 const ADDRESS = require('../helpers/constant/addresses.js');
-
+const _USDT = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+const _DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+const chainId = 1;
 
 async function test() {
     //  1 ETH => USDT
     // 先node start.js启动服务
-    const quote1 = await axios.get('http://localhost:3000/quote_0x', {       
+    const quote1 = await axios.get('http://localhost:3000/0x/quote', {
         params: {
             source_token: ADDRESS.ETH,
-            target_token: ADDRESS.USDT,
+            target_token: _USDT,
             amount: String(1e18),
             slippage: 100,
-            address: ADDRESS.WALLET
+            address: ADDRESS.WALLET,
+            chainId
         }
     });
 
-    const USDT = new ethers.Contract(ADDRESS.USDT, ERC20ABI, wallet);
+    const USDT = new ethers.Contract(_USDT, ERC20ABI, wallet);
 
     const defaultAddress = wallet.address;
     console.log('Before ETH balance:	', (await wallet.getBalance()) / 1e18);
     console.log('Before USDT balance:	', (await USDT.balanceOf(defaultAddress)) / 1e6);
 
     const tx = {
-        to: config['0x_ADDRESS'],
-        data: quote1.data.tx_data,       
+        to: quote1.data.to,
+        data: quote1.data.tx_data,
         value: ethers.utils.parseEther('1.0')
     };
     console.log(await wallet.sendTransaction(tx));
     console.log('After ETH balance:	', (await wallet.getBalance()) / 1e18);
     console.log('After USDT balance:	', (await USDT.balanceOf(defaultAddress)) / 1e6);
 
-
     //  USDT => DAI
-    const quote2 = await axios.get('http://localhost:3000/quote_0x', {       
+    const quote2 = await axios.get('http://localhost:3000/0x/quote', {
         params: {
-            source_token: ADDRESS.USDT,
-            target_token: ADDRESS.DAI,
-            amount: String(1000e6),
+            source_token: _USDT,
+            target_token: _DAI,
+            amount: String(1e5),
             slippage: 100,
-            address: ADDRESS.WALLET
+            address: ADDRESS.WALLET,
+            chainId
         }
     });
     const tx2 = {
-        to: config['0x_ADDRESS'],
-        data: quote2.data.tx_data       
+        to: quote2.data.to,
+        data: quote2.data.tx_data
     };
-
-    await USDT.approve(config['0x_ADDRESS'],String(1000e6));
-    
+    const rep = await USDT.approve(quote2.data.to, String(1e5));
+    await rep.wait();
     console.log(await wallet.sendTransaction(tx2));
-    const DAI = new ethers.Contract(ADDRESS.DAI, ERC20ABI, wallet);
+    const DAI = new ethers.Contract(_DAI, ERC20ABI, wallet);
 
     console.log('After USDT balance:	', (await USDT.balanceOf(defaultAddress)) / 1e6);
     console.log('After DAI balance:	', (await DAI.balanceOf(defaultAddress)) / 1e18);
     return;
 }
-
-
-
 
 test();
