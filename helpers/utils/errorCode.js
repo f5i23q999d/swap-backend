01b1d9066656906028aa601d8be5c5810007aebc
Kunhong.Li <knhongli@gmail.com>
2023-11-08 11:51:16 +0800

    update: return 400 status when error happened.    --FX2193

--- helpers/utils/errorCode.js
+++ helpers/utils/errorCode.js
@@ -1,26 +1,26 @@
 module.exports = {
   40000: {
-    statusCode: 500,
+    statusCode: 400,
     msg: 'invalid inputAmounts'
   },
   40001: {
-    statusCode: 500,
+    statusCode: 400,
     msg: 'source_token should not same as target_token'
   },
   40002: {
-    statusCode: 500,
+    statusCode: 400,
     msg: 'source_token should not same as target_token'
   },
   40003: {
-    statusCode: 500,
+    statusCode: 400,
     msg: 'unsupported chain'
   },
   40047: {
-    statusCode: 500,
+    statusCode: 400,
     msg: 'Service is busy. Please try again later.'
   },
   40048: {
-    statusCode: 500,
+    statusCode: 400,
     msg: 'Amount is too small.'
   }
 };
